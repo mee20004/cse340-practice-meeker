4857a4711a3dfaf8fb1d577677698afce2131bb4
mee20004 <mee20004@byui.edu>
2026-04-29 21:05:47 -0600

    Day1

--- server.js
+++ server.js
@@ -1,17 +1,65 @@
 // Import express using ESM syntax
 import express from 'express';
 
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const NODE_ENV = process.env.NODE_ENV || 'production';
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Create an instance of an Express application
 const app = express();
 
+/**
+ * Configure Express middleware
+ */
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 const name = process.env.NAME; // <-- NEW
 
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+
+
+/**
+ * Routes
+ */
 app.get('/', (req, res) => {
-    res.send(`Hello, ${name}!`); // <-- UPDATED
+    const title = 'Welcome Home';
+    res.render('home', { title });
 });
 
-// Define the port number the server will listen on
-const PORT = 3000;
+app.get('/about', (req, res) => {
+    const title = 'About Me';
+    res.render('about', { title });
+});
+
+
+app.get('/products', (req, res) => {
+    const title = 'Our Products';
+    res.render('products', { title });
+});
+
+app.get('/student', (req, res) => {
+    const student = {
+        title: 'Student Information',
+        name: 'Kameron Meeker',
+        id: '123456',
+        email: 'kameron.meeker@byui.edu',
+        address: '123 Main St, Rexburg, ID 83440'
+    };
+    res.render('student', student);
+});
+
+
 
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
